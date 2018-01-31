b81c72a563dd9790e9f9a11f3643a682445017e0
AokiSky <1249988950@qqcom>
2018-02-01 00:09:01 +0800

    FirstUpdate
    第一次尝试更新Github上的代码

--- src/firstGitHub.java
+++ src/firstGitHub.java
@@ -2,6 +2,7 @@ package test.javase;
 
 public class firstGitHub {
 	public static void main(String[] args) {
-		System.out.println("Hello Github");
+        String s = ",Hello Java";
+		System.out.println("Hello Github" + s);
 	}
 }
